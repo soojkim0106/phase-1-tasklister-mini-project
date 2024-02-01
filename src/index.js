eddcd1b060a8a290a2b43242a281e7204a8a6ecb
Soojungkim <soojung.kimm@gmail.com>
2024-02-01 14:57:36 -0800

    adjusted btn with css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     submitBtn(e.target.newTask.value)
+    e.target.reset()
   })
 })
 
@@ -12,10 +13,11 @@ function submitBtn(todo){
   let p = document.createElement('p');
   let btn = document.createElement('btn');
   p.innerText = `${todo}`
-  btn.textContent = '  x '
+  btn.textContent = ' x '
   document.querySelector('#tasks').append(p)
   p.appendChild(btn)
   btn.addEventListener('click', handleDelete)
+
 }
 
 function handleDelete(e){
