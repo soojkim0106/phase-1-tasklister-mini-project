63799d282c52f92314616d548a06f3bd3475022c
Soojungkim <soojung.kimm@gmail.com>
2024-02-01 12:28:38 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitBtn(e.target.newTask.value)
+  })
+})
+
+function submitBtn(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('btn');
+  p.innerText = `${todo}`
+  btn.textContent = '  x '
+  document.querySelector('#tasks').append(p)
+  p.appendChild(btn)
+  btn.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
